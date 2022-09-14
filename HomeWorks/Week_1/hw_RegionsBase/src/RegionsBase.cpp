ed5b715228a3108dd170c4819a20caa0237ab88f
Dmitry Morozov <9984398@gmail.com>
2022-09-15 00:55:22 +0300

    RegionsBase : Done Tested : PASSED Doc : Done

--- HomeWorks/Week_1/hw_RegionsBase/src/RegionsBase.cpp
+++ HomeWorks/Week_1/hw_RegionsBase/src/RegionsBase.cpp
@@ -12,11 +12,26 @@
 
 using namespace std;
 
+
+/**
+ * It compares two regions by comparing their standard names, then their parent standard names, then their names, and
+ * finally their populations
+ *
+ * @param lhs left-hand side operand
+ * @param rhs right-hand side
+ */
 bool operator< (const Region &lhs, const Region &rhs) {
     return tie (lhs.std_name, lhs.parent_std_name, lhs.names, lhs.population) <
            tie (rhs.std_name, rhs.parent_std_name, rhs.names, rhs.population);
 }
 
+/**
+ * It creates a map of regions to their repetition counts, and then finds the maximum repetition count
+ *
+ * @param regions a vector of Region objects.
+ *
+ * @return The maximum number of times a region is repeated in the vector.
+ */
 int FindMaxRepetitionCount (const vector<Region> &regions) {
     int result = 0;
     map<Region, int> repetition_count;
