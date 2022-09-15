bf477a9c7b3c68f2f0c2275b3040cc6a3a5326da
Dmitry Morozov <9984398@gmail.com>
2022-09-15 21:56:06 +0300

    Week_1 : TaskTracker : Draft implemented.

--- HomeWorks/Week_1/hw_TaskTracker/src/Team_Tasks.cpp
+++ HomeWorks/Week_1/hw_TaskTracker/src/Team_Tasks.cpp
@@ -4,8 +4,48 @@
 
 #include "shared/Team_Tasks.hpp"
 
+#include <string>
+
 
 void TeamTasks::AddNewTask (const std::string &person) {
-    ++tasks[person][TaskStatus::NEW];
+    tasks[person][TaskStatus::NEW]++;
+
+}
+
+const TasksInfo &TeamTasks::GetPersonTasksInfo (const std::string &person) const {
+    return tasks.at (person);
+}
+
+std::tuple<TasksInfo, TasksInfo> TeamTasks::PerformPersonTasks (const std::string &person, int task_count) {
+    TasksInfo updated_tasks, untouched_tasks;
+    if (tasks.count (person) == 0) {
+        return {updated_tasks, untouched_tasks};
+    }
+
+    TasksInfo &person_tasks = tasks[person];
+    for (auto &task : person_tasks) {
+        TaskStatus status = task.first;
+        int count = task.second;
+
+        auto next_status = static_cast<TaskStatus> (static_cast<int> (status) + 1);
+        int next_status_count = (next_status == TaskStatus::DONE) ? count : std::min (count, task_count);
+
+        task_count -= next_status_count;
+        person_tasks[next_status] += next_status_count;
+        person_tasks[status] -= next_status_count;
+
+        if (next_status_count > 0) {
+            updated_tasks[next_status] += next_status_count;
+        }
+        if (person_tasks[status] > 0) {
+            untouched_tasks[status] += person_tasks[status];
+        }
+    }
+
+    if (person_tasks[TaskStatus::DONE] == 0) {
+        tasks.erase (person);
+    }
+
+    return {updated_tasks, untouched_tasks};
 
 }
--- HomeWorks/Week_1/hw_TaskTracker/src/main.cpp
+++ HomeWorks/Week_1/hw_TaskTracker/src/main.cpp
@@ -5,16 +5,44 @@
 #include "shared/Team_Tasks.hpp"
 
 #include <iostream>
+#include <tuple>
 
+// Принимаем словарь по значению, чтобы иметь возможность
+// обращаться к отсутствующим ключам с помощью [] и получать 0,
+// не меняя при этом исходный словарь
+void PrintTasksInfo(TasksInfo tasks_info) {
+    std::cout << tasks_info[TaskStatus::NEW] << " new tasks" <<
+         ", " << tasks_info[TaskStatus::IN_PROGRESS] << " tasks in progress" <<
+         ", " << tasks_info[TaskStatus::TESTING] << " tasks are being tested" <<
+         ", " << tasks_info[TaskStatus::DONE] << " tasks are done" << std::endl;
+}
 
-/**
- * @brief Main function
- * @return 0
- */
-int main () {
-    std::cout << "Week_1\n\nTask : Task Tracker" << std::endl;
+int main() {
+    TeamTasks tasks;
+    tasks.AddNewTask("Ilia");
+    for (int i = 0; i < 3; ++i) {
+        tasks.AddNewTask("Ivan");
+    }
+    std::cout << "Ilia's tasks: ";
+    PrintTasksInfo(tasks.GetPersonTasksInfo("Ilia"));
+    std::cout << "Ivan's tasks: ";
+    PrintTasksInfo(tasks.GetPersonTasksInfo("Ivan"));
 
+    TasksInfo updated_tasks, untouched_tasks;
+
+    std::tie(updated_tasks, untouched_tasks) =
+            tasks.PerformPersonTasks("Ivan", 2);
+    std::cout << "Updated Ivan's tasks: ";
+    PrintTasksInfo(updated_tasks);
+    std::cout << "Untouched Ivan's tasks: ";
+    PrintTasksInfo(untouched_tasks);
+
+    std::tie(updated_tasks, untouched_tasks) =
+            tasks.PerformPersonTasks("Ivan", 2);
+    std::cout << "Updated Ivan's tasks: ";
+    PrintTasksInfo(updated_tasks);
+    std::cout << "Untouched Ivan's tasks: ";
+    PrintTasksInfo(untouched_tasks);
 
     return 0;
 }
-
