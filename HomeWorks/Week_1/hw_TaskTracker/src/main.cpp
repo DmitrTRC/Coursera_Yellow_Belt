57e76d301e7760d24726f9120f55c326f234af53
Dmitry Morozov <9984398@gmail.com>
2022-09-17 21:21:26 +0300

    TODO : FIX TaskTracker :  Fixing Tests : 6 Wrong lines

--- HomeWorks/Week_1/hw_TaskTracker/src/main.cpp
+++ HomeWorks/Week_1/hw_TaskTracker/src/main.cpp
@@ -22,17 +22,17 @@ void PrintTasksInfo (TasksInfo tasks_info) {
 void PrintTasksChanges (const TasksInfo &tasks_info) {
 
     for (const auto &[type, count]: tasks_info) {
-        if (tasks_info.count(TaskStatus::NEW) > 0) {
-            std::cout << count << " " << " new tasks" << std::endl;
+        if (tasks_info.count (TaskStatus::NEW) > 0) {
+            std::cout << "NEW : " << count;
         }
-        if (tasks_info.count( TaskStatus::IN_PROGRESS) > 0) {
-            std::cout << count << " " << " tasks in progress" << std::endl;
+        if (tasks_info.count (TaskStatus::IN_PROGRESS) > 0) {
+            std::cout << "IN_PROGRESS : " << count << " ,";
         }
         if (tasks_info.count (TaskStatus::TESTING) > 0) {
-            std::cout << count << " " << " tasks are being tested" << std::endl;
+            std::cout << "TESTING : " << count << " ,";
         }
-        if (tasks_info.count  (TaskStatus::DONE) > 0) {
-            std::cout << count << " " << " tasks are done" << std::endl;
+        if (tasks_info.count (TaskStatus::DONE) > 0) {
+            std::cout << "DONE : " << count << " ";
         }
     }
 }
@@ -44,18 +44,96 @@ int main () {
     for (int i = 0; i < 5; ++i) {
         tasks.AddNewTask ("Alice");
     }
+    std::cout << "Added 5 tasks for Alice. " << std::endl;
 
     PrintTasksInfo (tasks.GetPersonTasksInfo ("Alice"));
     std::cout << "----------------------------------" << std::endl;
 
     auto [updated, untouched] = tasks.PerformPersonTasks ("Alice", 5);
+    std::cout << "Perform Alice 5 tasks" << std::endl;
     PrintTasksInfo (tasks.GetPersonTasksInfo ("Alice"));
 
-    std::cout << "Updated ";
+    std::cout << "[{";
     PrintTasksChanges (updated);
-    std::cout << "Untouched ";
+    std::cout << "}, {";
     PrintTasksChanges (untouched);
+    std::cout << "}]" << std::endl;
     std::cout << std::endl;
+
+    std::cout << "New Person statuses : ";
+    PrintTasksInfo (tasks.GetPersonTasksInfo ("Alice"));
+    std::cout << "----------------------------------" << std::endl;
+
+    auto [updated2, untouched2] = tasks.PerformPersonTasks ("Alice", 5);
+    std::cout << "Perform Alice 5 tasks" << std::endl;
+    PrintTasksInfo (tasks.GetPersonTasksInfo ("Alice"));
+
+    std::cout << "[{";
+    PrintTasksChanges (updated2);
+    std::cout << "}, {";
+    PrintTasksChanges (untouched2);
+    std::cout << "}]" << std::endl;
+    std::cout << std::endl;
+
+    std::cout << "New Person statuses : ";
+    PrintTasksInfo (tasks.GetPersonTasksInfo ("Alice"));
+    std::cout << "----------------------------------" << std::endl;
+
+    //TODO: FIXME: Do not append 1 task do DONE status from updated
+    auto [updated3, untouched3] = tasks.PerformPersonTasks ("Alice", 1);
+    std::cout << "Perform Alice 1 tasks" << std::endl;
+    PrintTasksInfo (tasks.GetPersonTasksInfo ("Alice"));
+
+    std::cout << "[{";
+    PrintTasksChanges (updated3);
+    std::cout << "}, {";
+    PrintTasksChanges (untouched3);
+    std::cout << "}]" << std::endl;
+    std::cout << std::endl;
+
+    std::cout << "New Person statuses : ";
+    PrintTasksInfo (tasks.GetPersonTasksInfo ("Alice"));
+    std::cout << "----------------------------------" << std::endl;
+
+    for (int i = 0; i < 5; ++i) {
+        tasks.AddNewTask ("Alice");
+    }
+    std::cout << "Add 5 new tasks for Alice" << std::endl;
+
+    auto [updated4, untouched4] = tasks.PerformPersonTasks ("Alice", 2);
+    std::cout << "Perform Alice 2 tasks" << std::endl;
+    PrintTasksInfo (tasks.GetPersonTasksInfo ("Alice"));
+
+    std::cout << "[{";
+    PrintTasksChanges (updated4);
+    std::cout << "}, {";
+    PrintTasksChanges (untouched4);
+    std::cout << "}]" << std::endl;
+    std::cout << std::endl;
+
+    std::cout << "New Person statuses : ";
+    PrintTasksInfo (tasks.GetPersonTasksInfo ("Alice"));
+    std::cout << "----------------------------------" << std::endl;
+    PrintTasksInfo (tasks.GetPersonTasksInfo ("Alice"));
+    std::cout << "----------------------------------" << std::endl;
+
+    auto [updated5, untouched5] = tasks.PerformPersonTasks ("Alice", 4);
+
+    std::cout << "Perform Alice 4 tasks" << std::endl;
+    PrintTasksInfo (tasks.GetPersonTasksInfo ("Alice"));
+
+    std::cout << "[{";
+    PrintTasksChanges (updated5);
+    std::cout << "}, {";
+    PrintTasksChanges (untouched5);
+    std::cout << "}]" << std::endl;
+    std::cout << std::endl;
+
+    std::cout << "New Person statuses : ";
+    PrintTasksInfo (tasks.GetPersonTasksInfo ("Alice"));
+    std::cout << "----------------------------------" << std::endl;
+
+    PrintTasksInfo (tasks.GetPersonTasksInfo ("Alice"));
     std::cout << "----------------------------------" << std::endl;
 
 
