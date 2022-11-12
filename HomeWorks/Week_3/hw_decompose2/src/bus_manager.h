e676597e5c30dfcaaa3bb07a837c35a16d73d580
Dmitry Morozov <9984398@gmail.com>
2022-11-12 22:12:06 +0300

    Decompose2 : bus_manager.h : Done

--- HomeWorks/Week_3/hw_decompose2/src/bus_manager.h
+++ HomeWorks/Week_3/hw_decompose2/src/bus_manager.h
@@ -5,4 +5,25 @@
 #ifndef COURSERA_YELLOW_BELT_MAIN_BUS_MANAGER_H
 #define COURSERA_YELLOW_BELT_MAIN_BUS_MANAGER_H
 
+#include "responses.h"
+
+#include <iostream>
+
+class BusManager {
+
+public:
+    void AddBus(const std::string &bus, const std::vector<std::string> &stops);
+
+    BusesForStopResponse GetBusesForStop(const std::string &stop) const;
+
+    StopsForBusResponse GetStopsForBus(const std::string &bus) const;
+
+    AllBusesResponse GetAllBuses() const;
+
+private:
+
+    std::map<std::string, std::vector<std::string>> buses_to_stops, stops_to_buses;
+
+};
+
 #endif //COURSERA_YELLOW_BELT_MAIN_BUS_MANAGER_H
