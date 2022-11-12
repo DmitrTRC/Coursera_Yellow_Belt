f517281d4a43a6f4f83981298bf53566799dde14
Dmitry Morozov <9984398@gmail.com>
2022-11-12 17:27:53 +0300

    Week3: hw_1 : Done Test : Passed
    Phone_number  : Done
    Tests : Not

---
+++ HomeWorks/Week_3/hw_1/src/sum_reverse_sort.h
@@ -0,0 +1,17 @@
+//
+// Created by Dmitry Morozov on 12/11/22.
+//
+
+
+#pragma once
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int Sum(int x, int y);
+
+string Reverse(string s);
+
+void Sort(vector<int> &nums);
