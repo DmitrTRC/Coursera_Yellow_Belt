f2110f128c157620aa8617c04a14ce35861ef791
Dmitry Morozov <9984398@gmail.com>
2022-11-12 21:49:04 +0300

    Decompose2 : responses.h : Done

--- HomeWorks/Week_3/hw_decompose2/src/responses.h
+++ HomeWorks/Week_3/hw_decompose2/src/responses.h
@@ -5,4 +5,30 @@
 #ifndef COURSERA_YELLOW_BELT_MAIN_RESPONSES_H
 #define COURSERA_YELLOW_BELT_MAIN_RESPONSES_H
 
+#include <map>
+#include <string>
+#include <vector>
+
+struct BusesForStopResponse {
+
+    std::vector<std::string> buses;
+};
+
+std::ostream &operator<<(std::ostream &os, const BusesForStopResponse &r);
+
+struct StopsForBusResponse {
+
+    std::string bus;
+    std::vector<std::pair<std::string, std::vector<std::string>>> stops;
+
+};
+
+std::ostream &operator<<(std::ostream &os, const StopsForBusResponse &r);
+
+struct AllBusesResponse {
+
+    std::map<std::string, std::vector<std::string>> buses;
+};
+
+
 #endif //COURSERA_YELLOW_BELT_MAIN_RESPONSES_H
