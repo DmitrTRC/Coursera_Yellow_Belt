64a483c89294b291275d8d0a50c416d57e0d2e7d
Dmitry Morozov <9984398@gmail.com>
2022-11-14 00:10:01 +0300

    Week 4 : delete_repeating : Done Tests :

--- HomeWorks/Week_4/delete_repeating/src/main.cpp
+++ HomeWorks/Week_4/delete_repeating/src/main.cpp
@@ -4,10 +4,31 @@
 
 #include <algorithm>
 #include <iostream>
+#include <vector>
 
+template<typename T>
+void RemoveDuplicates(std::vector<T> &elements) {
+
+    std::sort(elements.begin(), elements.end());
+    auto it = std::unique(elements.begin(), elements.end());
+    elements.erase(it, elements.end());
+}
 
 int main() {
 
-    std::cout << "Hello, New Project !" << std::endl;
+    std::vector<int> v1 = {6, 4, 7, 6, 4, 4, 0, 1};
+    RemoveDuplicates(v1);
+    for (int x: v1) {
+        std::cout << x << " ";
+    }
+    std::cout << std::endl;
+
+    std::vector<std::string> v2 = {"C", "C++", "C++", "C", "C++"};
+    RemoveDuplicates(v2);
+    for (const std::string &s: v2) {
+        std::cout << s << " ";
+    }
+    std::cout << std::endl;
+
     return 0;
 }
