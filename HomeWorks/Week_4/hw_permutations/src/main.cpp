2b82624e781aec3e797d3d968a00854f62194589
Dmitry Morozov <9984398@gmail.com>
2022-11-14 00:24:24 +0300

    Week 4 : Draft permutation : Initial commit Tests :

--- HomeWorks/Week_4/hw_permutations/src/main.cpp
+++ HomeWorks/Week_4/hw_permutations/src/main.cpp
@@ -2,10 +2,29 @@
 // Created by Dmitry Morozov on 20/9/22.
 //
 
+#include <algorithm>
 #include <iostream>
+#include <vector>
 
 
-int main () {
-    std::cout << "Hello, New Project !" << std::endl;
+auto getPermutations(int n) {
+
+    std::vector<std::vector<int>> permutations;
+    std::vector<int> permutation(n);
+    for (int i = 0; i < n; ++i) {
+        permutation[i] = i + 1;
+    }
+    do {
+        permutations.push_back(permutation);
+    } while (std::prev_permutation(permutation.begin(), permutation.end()));
+    return permutations;
+}
+
+int main() {
+
+    int N;
+    std::cin >> N;
+
+
     return 0;
 }
