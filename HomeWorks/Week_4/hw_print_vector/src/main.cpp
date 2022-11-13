3d471515b83e33f2d6d77f42656fedf52eb7c2fe
Dmitry Morozov <9984398@gmail.com>
2022-11-13 08:55:08 +0300

    Week 4 : print_vector : Done

---
+++ HomeWorks/Week_4/hw_print_vector/src/main.cpp
@@ -0,0 +1,14 @@
+//
+// Created by Dmitry Morozov on 20/9/22.
+//
+
+#include <iostream>
+#include <vector>
+
+void PrintVectorPart(const std::vector<int>& numbers){
+    auto it = std::find_if(numbers.begin(), numbers.end(), [](int x){return x < 0;});
+    while (it != numbers.begin()){
+        std::cout << *(--it) << " ";
+    }
+}
+
