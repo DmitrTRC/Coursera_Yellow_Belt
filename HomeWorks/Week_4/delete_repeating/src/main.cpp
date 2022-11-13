48a6c6ff41040ce1c41ce1436aee50eb4cb9a3d6
Dmitry Morozov <9984398@gmail.com>
2022-11-14 00:04:52 +0300

    Week 4 : delete_repeating : Initial commit Tests :

---
+++ HomeWorks/Week_4/delete_repeating/src/main.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Dmitry Morozov on 20/9/22.
+//
+
+#include <algorithm>
+#include <iostream>
+
+
+int main() {
+
+    std::cout << "Hello, New Project !" << std::endl;
+    return 0;
+}
