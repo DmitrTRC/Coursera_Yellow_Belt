7418026a0e03c61f6dc9302f249c72240982b95b
Dmitry Morozov <9984398@gmail.com>
2022-11-14 00:16:32 +0300

    Week 4 : delete_repeating : Initial commit Tests : Passed

--- HomeWorks/Week_4/delete_repeating/src/main.cpp
+++ HomeWorks/Week_4/delete_repeating/src/main.cpp
@@ -14,21 +14,21 @@ void RemoveDuplicates(std::vector<T> &elements) {
     elements.erase(it, elements.end());
 }
 
-int main() {
-
-    std::vector<int> v1 = {6, 4, 7, 6, 4, 4, 0, 1};
-    RemoveDuplicates(v1);
-    for (int x: v1) {
-        std::cout << x << " ";
-    }
-    std::cout << std::endl;
-
-    std::vector<std::string> v2 = {"C", "C++", "C++", "C", "C++"};
-    RemoveDuplicates(v2);
-    for (const std::string &s: v2) {
-        std::cout << s << " ";
-    }
-    std::cout << std::endl;
-
-    return 0;
-}
+//int main() {
+//
+//    std::vector<int> v1 = {6, 4, 7, 6, 4, 4, 0, 1};
+//    RemoveDuplicates(v1);
+//    for (int x: v1) {
+//        std::cout << x << " ";
+//    }
+//    std::cout << std::endl;
+//
+//    std::vector<std::string> v2 = {"C", "C++", "C++", "C", "C++"};
+//    RemoveDuplicates(v2);
+//    for (const std::string &s: v2) {
+//        std::cout << s << " ";
+//    }
+//    std::cout << std::endl;
+//
+//    return 0;
+//}
