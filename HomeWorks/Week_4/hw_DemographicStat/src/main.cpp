a492cdb7cb98b245a1d56f5d8772cce9f95cd896
Dmitry Morozov <9984398@gmail.com>
2022-11-14 10:17:49 +0300

    Week 4 :
    Demo_Stat : Initial commit update
    Tests :

--- HomeWorks/Week_4/hw_DemographicStat/src/main.cpp
+++ HomeWorks/Week_4/hw_DemographicStat/src/main.cpp
@@ -3,9 +3,53 @@
 //
 
 #include <iostream>
+#include <algorithm>
+#include <vector>
+
+enum class Gender {
+    FEMALE,
+    MALE
+};
+
+struct Person {
+    int age;  // возраст
+    Gender gender;  // пол
+    bool is_employed;  // имеет ли работу
+};
+
+template<typename InputIt>
+int ComputeMedianAge(InputIt range_begin, InputIt range_end);
+
+///output
+//Median age = 40
+//Median age for females = 40
+//Median age for males = 55
+//Median age for employed females = 40
+//Median age for unemployed females = 80
+//Median age for employed males = 55
+//Median age for unemployed males = 78
+void PrintStats(std::vector<Person> persons) {
 
 
-int main () {
-    std::cout << "Hello, New Project !" << std::endl;
+
+
+
+}
+
+int main() {
+
+    std::vector<Person> persons = {
+            {31, Gender::MALE,   false},
+            {40, Gender::FEMALE, true},
+            {24, Gender::MALE,   true},
+            {20, Gender::FEMALE, true},
+            {80, Gender::FEMALE, false},
+            {78, Gender::MALE,   false},
+            {10, Gender::FEMALE, false},
+            {55, Gender::MALE,   true},
+    };
+
+    PrintStats(persons);
+
     return 0;
 }
