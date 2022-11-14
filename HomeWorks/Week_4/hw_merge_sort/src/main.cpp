2daa3fce16a40821916853c7ce5e9e7aa90e4d0e
Dmitry Morozov <9984398@gmail.com>
2022-11-14 21:27:10 +0300

    Week 4 : Merge_Sort : Done Tests :

---
+++ HomeWorks/Week_4/hw_merge_sort/src/main.cpp
@@ -0,0 +1,35 @@
+//
+// Created by Dmitry Morozov on 20/9/22.
+//
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+template<typename RandomIt>
+void MergeSort(RandomIt range_begin, RandomIt range_end) {
+
+    if (range_end - range_begin < 2) {
+        return;
+    }
+
+    std::vector<typename RandomIt::value_type> elements(range_begin, range_end);
+
+    auto it = elements.begin() + elements.size() / 2;
+
+    MergeSort(elements.begin(), it);
+    MergeSort(it, elements.end());
+
+    std::merge(elements.begin(), it, it, elements.end(), range_begin);
+
+}
+
+//int main() {
+//
+//    std::vector<int> v = {6, 4, 7, 6, 4, 4, 0, 1, 5};
+//    MergeSort(begin(v), end(v));
+//    for (int x: v) {
+//        std::cout << x << " ";
+//    }
+//    std::cout << std::endl;
+//    return 0;
+//}
