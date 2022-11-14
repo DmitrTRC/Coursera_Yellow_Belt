b896d21c904b3c9d6c3d10344824e064cca996d4
Dmitry Morozov <9984398@gmail.com>
2022-11-14 21:31:36 +0300

    Week 4 : Merge_Sort II: Done Tests : Passes
    Merge_Sort III:

--- HomeWorks/Week_4/hw_merge_sort/src/main.cpp
+++ HomeWorks/Week_4/hw_merge_sort/src/main.cpp
@@ -23,13 +23,13 @@ void MergeSort(RandomIt range_begin, RandomIt range_end) {
 
 }
 
-//int main() {
-//
-//    std::vector<int> v = {6, 4, 7, 6, 4, 4, 0, 1, 5};
-//    MergeSort(begin(v), end(v));
-//    for (int x: v) {
-//        std::cout << x << " ";
-//    }
-//    std::cout << std::endl;
-//    return 0;
-//}
+int main() {
+
+    std::vector<int> v = {6, 4, 7, 6, 4, 4, 0, 1, 5};
+    MergeSort(begin(v), end(v));
+    for (int x: v) {
+        std::cout << x << " ";
+    }
+    std::cout << std::endl;
+    return 0;
+}
