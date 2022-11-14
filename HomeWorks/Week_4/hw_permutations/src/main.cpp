c0784167ec6a389d58378c4a081ae97c67f0f9bf
Dmitry Morozov <9984398@gmail.com>
2022-11-14 09:05:43 +0300

    Week 4 : Draft permutation : Done Tests :

--- HomeWorks/Week_4/hw_permutations/src/main.cpp
+++ HomeWorks/Week_4/hw_permutations/src/main.cpp
@@ -11,12 +11,15 @@ auto getPermutations(int n) {
 
     std::vector<std::vector<int>> permutations;
     std::vector<int> permutation(n);
-    for (int i = 0; i < n; ++i) {
-        permutation[i] = i + 1;
+
+    for (int i = n; i != 0; --i) {
+        permutation[n - i] = i;
     }
+
     do {
         permutations.push_back(permutation);
     } while (std::prev_permutation(permutation.begin(), permutation.end()));
+
     return permutations;
 }
 
@@ -25,6 +28,13 @@ int main() {
     int N;
     std::cin >> N;
 
+    auto permutations = getPermutations(N);
+    for (const auto &permutation: permutations) {
+        for (int x: permutation) {
+            std::cout << x << " ";
+        }
+        std::cout << std::endl;
+    }
 
     return 0;
 }
