467456cd4aaf320c0895c840ccbd3a261a8ab6aa
Dmitry Morozov <9984398@gmail.com>
2022-11-18 21:44:58 +0300

    Week 4 :
    Personal finances : Done
    Tests : None

--- HomeWorks/Week_4/hw_PersonalFinance/src/main.cpp
+++ HomeWorks/Week_4/hw_PersonalFinance/src/main.cpp
@@ -15,6 +15,18 @@
 
 ///Convert string "YYYY-MM-DD" to date
 
+std::chrono::year_month_day convertStringToDate(const std::string &date) {
+
+    std::tm tm = {};
+    std::istringstream ss(date);
+    ss >> std::get_time(&tm, "%Y-%m-%d");
+    if (ss.fail()) {
+        throw std::runtime_error("Wrong date format");
+    }
+
+    return std::chrono::year_month_day(std::chrono::year(tm.tm_year + 1900), std::chrono::month(tm.tm_mon + 1),
+                                       std::chrono::day(tm.tm_mday));
+}
 
 class Earn {
 public:
@@ -61,38 +73,50 @@ public:
 
     [[nodiscard]] double computeIncome(const std::string &from, const std::string &to) const {
 
+        auto from_date = convertStringToDate(from);
+        auto to_date = convertStringToDate(to);
+
+        if (from_date > to_date) {
+            throw std::invalid_argument("from > to");
+        }
+
+        double income = 0;
+
+        for (const auto &earn: earns_) {
+            if (earn.from() <= from_date && earn.to() >= to_date) {
+                income += getEarnPerDay(earn) * getDaysBetweenDates(from_date, to_date);
+            } else if (earn.from() <= from_date && earn.to() >= from_date) {
+                income += getEarnPerDay(earn) * getDaysBetweenDates(from_date, earn.to());
+            } else if (earn.from() <= to_date && earn.to() >= to_date) {
+                income += getEarnPerDay(earn) * getDaysBetweenDates(earn.from(), to_date);
+            } else if (earn.from() >= from_date && earn.to() <= to_date) {
+                income += getEarnPerDay(earn) * getDaysBetweenDates(earn.from(), earn.to());
+            }
+        }
+
+        return income;
+
     }
 
-    double getEarnPerDay(const Earn &earn) {
+    [[nodiscard]] static double getEarnPerDay(const Earn &earn) {
 
         return earn.value() / ((std::chrono::sys_days(earn.to())).time_since_epoch() -
                                (std::chrono::sys_days(earn.from())).time_since_epoch()).count();
-
-
     }
 
 private:
     std::vector<Earn> earns_;
 
 
-    int getDaysBetween(const std::string &from, const std::string &to) const {
+    static int getDaysBetweenDates(std::chrono::year_month_day from, std::chrono::year_month_day to) {
+
+        return ((std::chrono::sys_days(to)).time_since_epoch() -
+                (std::chrono::sys_days(from)).time_since_epoch()).count();
     }
 
 
 };
 
-std::chrono::year_month_day convertStringToDate(const std::string &date) {
-
-    std::tm tm = {};
-    std::istringstream ss(date);
-    ss >> std::get_time(&tm, "%Y-%m-%d");
-    if (ss.fail()) {
-        throw std::runtime_error("Wrong date format");
-    }
-
-    return std::chrono::year_month_day(std::chrono::year(tm.tm_year + 1900), std::chrono::month(tm.tm_mon + 1),
-                                       std::chrono::day(tm.tm_mday));
-}
 
 void commander(int nCommands) {
 
