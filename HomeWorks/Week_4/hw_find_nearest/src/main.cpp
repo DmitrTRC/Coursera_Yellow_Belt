22443139fc1c6713333d294fca7913f392303e41
Dmitry Morozov <9984398@gmail.com>
2022-11-21 23:40:20 +0300

    Week 4 :
    Find Nearest : Draft Done
    Tests :

--- HomeWorks/Week_4/hw_find_nearest/src/main.cpp
+++ HomeWorks/Week_4/hw_find_nearest/src/main.cpp
@@ -3,9 +3,46 @@
 //
 
 #include <iostream>
+#include <set>
+
+std::set<int>::const_iterator FindNearestElement(
+        const std::set<int>& numbers,
+        int border) {
+
+    if (numbers.empty()) {
+        return numbers.end();
+
+    }
+
+    auto it = numbers.lower_bound(border);
+
+if (it == numbers.begin()) {
+        return it;
+    }
+
+    if (it == numbers.end()) {
+        return std::prev(it);
+    }
+
+    auto left = std::prev(it);
+
+    if (border - *left <= *it - border) {
+        return left;
+    }
+
+    return it;
+
+}
+//int main () {
+//    std::set<int> numbers = {1, 4, 8, 9, 7, 5};
+//
+//    std::cout << *FindNearestElement(numbers, 0) << std::endl;
+//    std::cout << *FindNearestElement(numbers, 3) << std::endl;
+//    std::cout << *FindNearestElement(numbers, 5) << std::endl;
+//    std::cout << *FindNearestElement(numbers, 6) << std::endl;
+//    std::cout << *FindNearestElement(numbers, 8) << std::endl;
+//    std::cout << *FindNearestElement(numbers, 100) << std::endl;
 
 
-int main () {
-    std::cout << "Hello, New Project !" << std::endl;
     return 0;
 }
