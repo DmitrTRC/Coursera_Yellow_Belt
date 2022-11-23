56e412f453dc2c2025e7ce1b583588b0cee3bce6
Dmitry Morozov <9984398@gmail.com>
2022-11-23 08:44:40 +0300

    Week 4 :
    Find_Prefix : Initial done
    Tests : Passes
    
    Expression : Initial commit

--- HomeWorks/Week_4/hw_prefix/src/main.cpp
+++ HomeWorks/Week_4/hw_prefix/src/main.cpp
@@ -3,9 +3,42 @@
 //
 
 #include <iostream>
+#include <vector>
+#include <algorithm>
+#include <iterator>
 
+using namespace std;
 
-int main () {
-    std::cout << "Hello, New Project !" << std::endl;
+template<typename RandomIt>
+pair<RandomIt, RandomIt> FindStartsWith(RandomIt range_begin, RandomIt range_end, char prefix) {
+    string prefix_str(1, prefix);
+    return equal_range(range_begin, range_end, prefix_str,
+                       [](const string &lhs, const string &rhs) {
+                           return lhs[0] < rhs[0];
+                       });
+
+}
+
+int main() {
+
+    const vector<string> sorted_strings = {"moscow", "murmansk", "vologda"};
+
+    const auto m_result =
+            FindStartsWith(begin(sorted_strings), end(sorted_strings), 'm');
+    for (auto it = m_result.first; it != m_result.second; ++it) {
+        cout << *it << " ";
+    }
+    cout << endl;
+
+    const auto p_result =
+            FindStartsWith(begin(sorted_strings), end(sorted_strings), 'p');
+    cout << (p_result.first - begin(sorted_strings)) << " " <<
+         (p_result.second - begin(sorted_strings)) << endl;
+
+    const auto z_result =
+            FindStartsWith(begin(sorted_strings), end(sorted_strings), 'z');
+    cout << (z_result.first - begin(sorted_strings)) << " " <<
+         (z_result.second - begin(sorted_strings)) << endl;
+
     return 0;
 }
