eb8589e2395595b59a52671003e42eea826f4b1e
Dmitry Morozov <9984398@gmail.com>
2023-01-13 13:56:27 +0300

    Week 6 :
    Node : Done
    Tests :

--- HomeWorks/Week_6/FinalProject/src/node.cpp
+++ HomeWorks/Week_6/FinalProject/src/node.cpp
@@ -3,3 +3,77 @@
 //
 
 #include "node.h"
+
+#include <utility>
+
+EmptyNode::EmptyNode() noexcept = default;
+
+bool EmptyNode::Evaluate(const Date &date, const std::string &event) const noexcept {
+
+    return true;
+}
+
+DateComparisonNode::DateComparisonNode(const Comparison &comprs, const Date &date) noexcept: cmpr(comprs), dt(date) {
+}
+
+bool DateComparisonNode::Evaluate(const Date &date, const std::string &event) const noexcept {
+
+    switch (cmpr) {
+        case Comparison::Less:
+            return date < dt;
+        case Comparison::LessOrEqual:
+            return date <= dt;
+        case Comparison::Greater:
+            return date > dt;
+        case Comparison::GreaterOrEqual:
+            return date >= dt;
+        case Comparison::Equal:
+            return date == dt;
+        case Comparison::NotEqual:
+            return date != dt;
+        default:
+            return false;
+    }
+}
+
+EventComparisonNode::EventComparisonNode(const Comparison &comprs, std::string event) : cmpr(comprs),
+                                                                                        evnt(std::move(event)) {
+}
+
+bool EventComparisonNode::Evaluate(const Date &date, const std::string &event) const noexcept {
+
+    switch (cmpr) {
+        case Comparison::Less:
+            return event < evnt;
+        case Comparison::LessOrEqual:
+            return event <= evnt;
+        case Comparison::Greater:
+            return event > evnt;
+        case Comparison::GreaterOrEqual:
+            return event >= evnt;
+        case Comparison::Equal:
+            return event == evnt;
+        case Comparison::NotEqual:
+            return event != evnt;
+        default:
+            return false;
+    }
+}
+
+LogicalOperationNode::LogicalOperationNode(const LogicalOperation &logical_operation,
+                                           const std::shared_ptr<Node> &lhs,
+                                           const std::shared_ptr<Node> &rhs) noexcept: lgc_op(logical_operation),
+                                                                                       lhs_node(lhs), rhs_node(rhs) {
+}
+
+bool LogicalOperationNode::Evaluate(const Date &date, const std::string &event) const {
+
+    switch (lgc_op) {
+        case LogicalOperation::Or:
+            return lhs_node->Evaluate(date, event) || rhs_node->Evaluate(date, event);
+        case LogicalOperation::And:
+            return lhs_node->Evaluate(date, event) && rhs_node->Evaluate(date, event);
+        default:
+            return false;
+    }
+}
--- HomeWorks/Week_6/FinalProject/src/node.h
+++ HomeWorks/Week_6/FinalProject/src/node.h
@@ -6,12 +6,71 @@
 #define COURSERA_YELLOW_BELT_DEV_NODE_H
 
 #include "date.h"
-#include "sstream"
 
-struct Node {
+#include <map>
+#include <set>
 
+enum class Comparison {
+    Less,
+    LessOrEqual,
+    Greater,
+    GreaterOrEqual,
+    Equal,
+    NotEqual
+};
+enum class LogicalOperation {
+    Or,
+    And
+};
+
+class Node {
+public:
     [[nodiscard]] virtual bool Evaluate(const Date &date, const std::string &event) const = 0;
 
+    virtual ~Node() = default;
+};
+
+class EmptyNode : public Node {
+public:
+    EmptyNode() noexcept;
+
+    [[nodiscard]] bool Evaluate(const Date &date, const std::string &event) const noexcept override;
+};
+
+class DateComparisonNode : public Node {
+public:
+    DateComparisonNode(const Comparison &comprs, const Date &date) noexcept;
+
+    [[nodiscard]] bool Evaluate(const Date &date, const std::string &event) const noexcept override;
+
+private:
+    const Comparison cmpr;
+    const Date dt;
+};
+
+class EventComparisonNode : public Node {
+public:
+    EventComparisonNode(const Comparison &comprs, std::string event);
+
+    [[nodiscard]] bool Evaluate(const Date &date, const std::string &event) const noexcept override;
+
+private:
+    const Comparison cmpr;
+    const std::string evnt;
+};
+
+class LogicalOperationNode : public Node {
+public:
+    LogicalOperationNode(const LogicalOperation &logical_operation,
+                         const std::shared_ptr<Node> &lhs,
+                         const std::shared_ptr<Node> &rhs) noexcept;
+
+    [[nodiscard]] bool Evaluate(const Date &date, const std::string &event) const override;
+
+private:
+    const LogicalOperation lgc_op;
+    const std::shared_ptr<Node> lhs_node;
+    const std::shared_ptr<Node> rhs_node;
 };
 
 
